88c47c6c7652837bf37c1556013f01129002ed4f
Sean Canton <eighteyes@gmail.com>
2017-12-18 15:01:59 -0800

    final patch

--- utils/Socket.js
+++ utils/Socket.js
@@ -4,102 +4,100 @@ var socket;
 function sendDeviceCommand(eventName, payload, cb, options) {
 
   // TODO: Parameter defaults in ES6
-  var options = ( _.isUndefined(options) ) ? {} : options;
+  var options = (_.isUndefined(options)) ? {} : options;
 
   // support 2 parameter form
-  if ( _.isFunction(payload)){
+  if (_.isFunction(payload)) {
     cb = payload;
   }
 
-  if ( _.isUndefined(payload) || _.isFunction(payload)){
+  if (_.isUndefined(payload) || _.isFunction(payload)) {
     payload = {};
   }
 
   _.extend(payload, { token: admatrix.config.device.token });
 
-    initSocket(function(err, io) {
-      if (err) return cb(err);
-      io.on('open', function() {
-        debug('socket open');
-        io.on('close', function(){
-          debug('socket close')
-          if ( _.isFunction(cb)) {
-            cb(null);
-          }
-        });
-        io.on('error', function(err){
-          debug('socket error')
-          if ( _.isFunction(cb)) {
-            cb(err);
-          }
-        })
-        io.on('message', function(msg) {
-          // console.log(msg);
-          try {
-            msg = JSON.parse(msg);
-          } catch (e) {
-            cb(e);
-          }
-          switch (msg.channel) {
-            case 'register-fail':
-              cb(new Error('client registration fail'));
-              break;
-            case 'register-ok':
-              debug('Register ok');
-              var theEvent = {};
-              //groups case
-              if(_.isArray(admatrix.config.device.identifier)) {               
-                async.map(admatrix.config.device.identifier, function (did, callback) {
-                  theEvent = {
-                    t: eventName,
-                    deviceId: did.identifier,
-                    p: payload
-                  };
-                  debug('[ss] Groups Start: ', theEvent);
-                  emitSocket('client-cmd', theEvent);
-                }, function(err) {
-                  if(err) cb(err);
-                })
-              } else {
-                var theEvent = {
+  initSocket(function (err, io) {
+    if (err) return cb(err);
+    io.on('open', function () {
+      debug('socket open');
+      io.on('close', function () {
+        debug('socket close')
+        if (_.isFunction(cb)) {
+          cb(null);
+        }
+      });
+      io.on('error', function (err) {
+        debug('socket error')
+        if (_.isFunction(cb)) {
+          cb(err);
+        }
+      })
+      io.on('message', function (msg) {
+        // console.log(msg);
+        try {
+          msg = JSON.parse(msg);
+        } catch (e) {
+          cb(e);
+        }
+        switch (msg.channel) {
+          case 'register-fail':
+            cb(new Error('client registration fail'));
+            break;
+          case 'register-ok':
+            debug('Register ok');
+            var theEvent = {};
+            //groups case
+            if (_.isArray(admatrix.config.device.identifier)) {
+              async.map(admatrix.config.device.identifier, function (did, callback) {
+                theEvent = {
                   t: eventName,
-                  deviceId: admatrix.config.device.identifier,
+                  deviceId: did.identifier,
                   p: payload
                 };
-                debug('[ss]', theEvent);
-              }
-              
+                debug('[ss] Groups Start: ', theEvent);
+                emitSocket('client-cmd', theEvent);
+              }, function (err) {
+                if (err) cb(err);
+              })
+            } else {
+              var theEvent = {
+                t: eventName,
+                deviceId: admatrix.config.device.identifier,
+                p: payload
+              };
+              debug('[ss]', theEvent);
               emitSocket('client-cmd', theEvent);
-              if ( options.keepOpen === true ) {
-                //do nothing
-              } else {
-                io.close();
-              }
-              break;
-            case 'app-log':
-            case 'server-message':
-              debug('[ss]>sdk'.green, msg);
-              if ( _.isFunction(cb)) {
-                cb(null, msg);
-              } else {
-                console.error('Server-Message not handled from:', eventName)
-              }
-              break;
-            default:
-              console.warn('Bad channel:', msg.channel);
-              break;
-          }
-        });
+            }
+
+            if (!options.keepOpen === true) {
+              io.close();
+            }
+            break;
+          case 'app-log':
+          case 'server-message':
+            debug('[ss]>sdk'.green, msg);
+            if (_.isFunction(cb)) {
+              cb(null, msg);
+            } else {
+              console.error('Server-Message not handled from:', eventName)
+            }
+            break;
+          default:
+            console.warn('Bad channel:', msg.channel);
+            break;
+        }
       });
     });
+  });
 }
 
-function initSocket(sUrl, cb){
-  if ( _.isFunction(sUrl)){
+function initSocket(sUrl, cb) {
+  if (_.isFunction(sUrl)) {
     cb = sUrl;
     sUrl = admatrix.config.url.streaming;
   }
-  if (!admatrix.config.user.hasOwnProperty('token') ){
+  if (!admatrix.config.user.hasOwnProperty('token')) {
     return cb('SDK needs Access Token. Login Again');
   }
 
@@ -107,44 +105,44 @@ function initSocket(sUrl, cb){
   sUrl = url.parse(sUrl);
   sUrl.protocol = 'wss';
   // if fixed ip, 0-9 use ws
-  if ( !_.isNaN( parseInt(sUrl[sUrl.length-1]) ) ){
+  if (!_.isNaN(parseInt(sUrl[sUrl.length - 1]))) {
     sUrl.protocol = 'ws';
   }
-  if ( sUrl.hostname.indexOf('localhost') > -1 ){
+  if (sUrl.hostname.indexOf('localhost') > -1) {
     sUrl.protocol = 'http'
   }
   // sUrl.pathname = 'engine.io';
 
   debug(sUrl)
 
-  debug('Trying Socket Connection:'.grey,  require('url').format(sUrl) );
+  debug('Trying Socket Connection:'.grey, require('url').format(sUrl));
 
-      var io = require('engine.io-client')(url.format(sUrl), {
-        transports: [ 'websocket', 'polling' ],
-      });
+  var io = require('engine.io-client')(url.format(sUrl), {
+    transports: ['websocket', 'polling'],
+  });
 
-      io.on('open', function(){
-        debug('Socket Connection:'.green,  require('url').format(sUrl) );
-        socket = io;
-        io.send(JSON.stringify({
-          channel : 'client-register',
-          payload: { userId: admatrix.config.user.id, userToken: admatrix.config.user.token }
-        }));
-      });
+  io.on('open', function () {
+    debug('Socket Connection:'.green, require('url').format(sUrl));
+    socket = io;
+    io.send(JSON.stringify({
+      channel: 'client-register',
+      payload: { userId: admatrix.config.user.id, userToken: admatrix.config.user.token }
+    }));
+  });
 
-      io.on('error', function(err){
-        console.log('socket error'.red, err);
-      });
+  io.on('error', function (err) {
+    console.log('socket error'.red, err);
+  });
 
-      io.on('register-fail', function(e){
-        console.log('registration fail', e);
-      });
+  io.on('register-fail', function (e) {
+    console.log('registration fail', e);
+  });
 
-      cb(null, io);
+  cb(null, io);
 
 }
 
-function emitSocket(channel, msg){
+function emitSocket(channel, msg) {
   debug('[ss]>', channel.blue, msg)
   socket.send(JSON.stringify({
     channel: channel, payload: msg
